bc488b72df49a04c6ce157d16e3a1870ef1f4776
Enes Demirag <enesdemirag1@hotmail.com>
2018-03-10 13:48:39 +0300

    Add new statements

--- notes/blur_sharp.py
+++ notes/blur_sharp.py
@@ -7,11 +7,11 @@ import cv2
 |1 1 1|
 
 #Box Filters
-kernel_3x3 = np.ones((3,3) , np.float32) / 9
+kernel_3x3 = np.ones((3,3) , np.float32) / 9 #Ned to divide 9 due to normalization
 kernel_5x5 = np.ones((5,5) , np.float32) / 25
 kernel_7x7 = np.ones((7,7) , no.float32) / 49
 
-#Blur - Normalized
+#Blur - Convolotion
 blurred = cv2.filter2D(image, -1, kernel_3x3)
 
 #Blur - Averaged
@@ -21,7 +21,17 @@ blured_box = cv2.blur(image, (3,3))
 blurred_gauss = cv2.GaussianBlur(image, (7,7), 0)
 
 #Blur Median
-blurred_median = cv2.medianBlur(image, 5) #Paintted type effect
+blurred_median = cv2.medianBlur(image, 5) #Painted type effect
 
 #Blur - Bilateral
-blurred_bileteral = cv2.bilateralFilter(image, 9, 75, 75)
+blurred_bileteral = cv2.bilateralFilter(image, 9, 75, 75) #Very effective noise but keeps edges sharp
+
+#Image Denoising
+#Nonlocal Means Denoising (Cleaning noise and making image more DSLR)
+dst = cv2.fastNlMeansDenoisingColored(image, None, 6, 6, 7, 21) # For color image
+      cv2.fastNlMeansDenoising()  # Works with single grayscale image
+      cv2.fastNlMeansDenoisingMulti() # works with image sequence captured in short period of time (grayscale)
+      cv2.fastNlMeansDenoisingColoredMulti() # works with image sequence captured in short period of time (color)
+
+
+#Sharpening
